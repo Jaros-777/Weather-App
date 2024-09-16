c85fb31a8767ffe583e68718f4c34cffb2996833
Jaros-777 <filip.jarocki@wp.pl>
2024-09-16 17:50:12 +0200

    Add functions to navbar

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,25 +1,27 @@
+import "./App.css";
+import sun from "./assets/icon-sun.png";
+import humidityIcon from "./assets/icon-humidity.png";
+import windIcon from "./assets/icon-wind.png";
+import searchIcon from "./assets/icon-search.png";
+import iconList from "./assets/icon-list.png";
+import { useEffect, useState } from "react";
 
-import './App.css'
-import sun from "./assets/icon-sun.png"
-import humidityIcon from "./assets/icon-humidity.png"
-import windIcon from "./assets/icon-wind.png"
-import searchIcon from "./assets/icon-search.png"
-import { useEffect, useState } from 'react'
+import NavBar from "./navBar.jsx";
 
 function App() {
-
-  const apiKey = "e277829c34c5f9721a82494c55ff26ff"
+  const apiKey = "YOUR API KEY";
 
   const [temp, setTemp] = useState(0);
   const [humidity, setHumidity] = useState(0);
   const [wind, setWind] = useState(0);
   const [icon, setIcon] = useState(0);
-  const val =`https://openweathermap.org/img/wn/${icon}@2x.png`
+  const val = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
-  const [city, setCity]= useState("London")
-  const [cityName, setCityName]= useState(city)
+  const [city, setCity] = useState("#");
+  const [cityName, setCityName] = useState(city);
 
-  const search = async(city)=> {
+
+  const search = async (city) => {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
@@ -30,67 +32,61 @@ function App() {
       setTemp(data.main.temp);
       setHumidity(data.main.humidity);
       setWind(data.wind.speed);
-      setIcon(data.weather[0].icon)
-
+      setIcon(data.weather[0].icon);
     } catch (error) {
-      setCityName("Town not found")
+      setCityName("Town not found");
       setTemp(0);
       setHumidity(0);
       setWind(0);
     }
-  }
+  };
 
-      useEffect(()=>{
-        search(city);
-        console.log(city)
-      },[])
+  useEffect(() => {
+    // search(city);
+    // console.log(city);
+  }, []);
 
-  function changeTown()
-  {
-    search(city) 
-    console.log({city}), 
-    setCityName(city)
-    console.log(icon)
+  function changeTown(City) {
+    setCity(City);
+    search(City);
+    setCityName(City);
+    console.log({ City });
+    console.log(icon);
   }
 
   return (
     <>
-    <div id='main-container' >
-      <div id='input-field'>
-        <input onKeyDown={(e) => {
-              if (e.key == "Enter"){
-                changeTown();
-              }
-            }} type="text" placeholder='Search' onChange={(e)=> {setCity(e.target.value)}}/>
-        <button onClick={changeTown} ><img src={searchIcon} alt="" /></button>
-      </div>
-      <div id='center'>
-        <img src={val} alt="sun" />
-        <div style={{display: "flex", alignItems: "end"}}>
-        <p style={{fontSize:"10vh"}} >{Math.floor(temp)}</p>
-        <p style={{fontSize:"9vh"}} ><sup>o</sup>c</p>
-        </div>
-        <p>{cityName}</p>
-      </div>
-      <div id='bottom'>
-        <div className='detail'>
-          <img src={humidityIcon} alt="cloud" />
-          <div className='detail-text'>
-            <p>{humidity}</p>
-            <p>Humidity</p>
+      <div id="main-container">
+        <NavBar changeTown={changeTown} city={city} setCity={setCity}></NavBar>
+        <div id="center">
+          <img src={val} alt={icon} />
+          <div style={{ display: "flex", alignItems: "end" }}>
+            <p style={{ fontSize: "9vh" }}>{Math.floor(temp)}</p>
+            <p style={{ fontSize: "8vh" }}>
+              <sup>o</sup>c
+            </p>
           </div>
+          <p>{cityName}</p>
         </div>
-        <div className='detail'>
-          <img src={windIcon} alt="wind" />
-          <div className='detail-text'>
-            <p>{wind} Km/h</p>
-            <p>Wind Speed</p>
+        <div id="bottom">
+          <div className="detail">
+            <img src={humidityIcon} alt="cloud" />
+            <div className="detail-text">
+              <p>{humidity}</p>
+              <p>Humidity</p>
+            </div>
+          </div>
+          <div className="detail">
+            <img src={windIcon} alt="wind" />
+            <div className="detail-text">
+              <p>{wind} Km/h</p>
+              <p>Wind Speed</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ weatherApp/src/navBar.jsx
@@ -0,0 +1,98 @@
+import "./navBar.css";
+import sun from "./assets/icon-sun.png";
+import searchIcon from "./assets/icon-search.png";
+import iconList from "./assets/icon-list.png";
+import emptyStar from "./assets/icon-empty-star.png";
+import fullStar from "./assets/icon-full-star.png";
+import trash from "./assets/icon-trash.png";
+import { v4 as uuid } from "uuid";
+import { useState } from "react";
+
+function NavBar(props) {
+  const [favouriteIcon, setFavouriteIcon] = useState(false);
+
+  const [favoriteLocation, setFavouriteLocation] = useState([
+    {
+      id: 1,
+      name: "Warszawa",
+      favourite: true,
+    },
+    {
+      id: 2,
+      name: "Tokio",
+      favourite: true,
+    },
+  ]);
+
+  function removeFavLoc(index) {
+    const newList = favoriteLocation.filter((e) => e.id !== index);
+    setFavouriteLocation(newList);
+  }
+
+  function addFavLoc() {
+    const newLoc = {
+      id: uuid(),
+      name: props.city,
+      favourite: true,
+    };
+    setFavouriteLocation([...favoriteLocation, newLoc])
+  }
+
+  return (
+    <>
+      <div id="top">
+        <div id="input-field">
+          <input
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                props.changeTown(props.city);
+              }
+            }}
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
+              props.setCity(e.target.value);
+            }}
+          />
+        </div>
+        <button onClick={addFavLoc} id="fav-btn">
+          <img src={favouriteIcon ? fullStar : emptyStar} alt="emptyStar" />
+        </button>
+        <button onClick={props.changeTown}>
+          <img src={searchIcon} alt="searchIcon" />
+        </button>
+        <div id="fav-location">
+          <li>
+            <img src={iconList} alt="iconList" />
+            <ul id="fav-location-list">
+              {favoriteLocation.length === 0 ? (
+                <p>Empty</p>
+              ) : (
+                favoriteLocation.map((l) => (
+                  <li key={l.id}>
+                    <button
+                      onClick={() => {
+                        props.changeTown(l.name);
+                      }}
+                    >
+                      {l.name}
+                    </button>
+                    <button
+                      onClick={() => {
+                        removeFavLoc(l.id);
+                      }}
+                    >
+                      <img src={trash} alt="trash" />
+                    </button>
+                  </li>
+                ))
+              )}
+            </ul>
+          </li>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
